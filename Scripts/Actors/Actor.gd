c54dfa7a5885aba47a25efcd08b68c1f4b5d8a62
jamnk <pancakes3825@gmail.com>
2023-10-25 23:06:57 -0400

    knight stuff

--- Scripts/Actors/Actor.gd
+++ Scripts/Actors/Actor.gd
@@ -1,7 +1,7 @@
 extends KinematicBody2D
 class_name Actor
 
-export var speed: = Vector2(5000, 5000)
+export var speed: = Vector2(4000, 4000)
 export var sprint_mult: = Vector2(1.2, 1.2)
 export var health: = 100
 export var armor: = 0
--- Scripts/Actors/NPCS/ENEMIES/EnemyKnights.gd
+++ Scripts/Actors/NPCS/ENEMIES/EnemyKnights.gd
@@ -1,9 +1,9 @@
 extends Actor
 
-export var walkrange = 200
+export var walkrange = 10000
 var target: Vector2
 onready var walktimer:= $walktimer
-onready var weapon:= $Weapon
+onready var weapon:= $Sword
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -15,16 +15,17 @@ func _ready():
 
 
 func _physics_process(delta):
-	weapon.rotation = position.angle_to_point(target)
-	if PlayerAutoload.knightsAgressive:
-		target = PlayerAutoload.playerPos
-		
 	velocity = position.direction_to(target) * speed
 	
-	if position.distance_to(target) > 20:
+	weapon.rotation = position.angle_to_point(target)
+	
+	if PlayerAutoload.knightsAgressive:
+		target = PlayerAutoload.playerPos
+	if position.distance_to(target) > 100:
 		move_and_slide(velocity)
 		if target == PlayerAutoload.playerPos:
 			attack()
+	
 	
 	
 	
