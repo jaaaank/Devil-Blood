df4a6d840422ed26823aaf0db6f439f88b8c3a6c
jamnk <pancakes3825@gmail.com>
2023-11-01 09:22:16 -0400

    I GEEET BOOOORRREEEDDDDDDD

--- Scripts/Actors/Objects/swordattack.gd
+++ Scripts/Actors/Objects/swordattack.gd
@@ -1,10 +1,23 @@
 extends Position2D
 
+var player = Node
+
+func _ready():
+	player = get_parent().get_parent().get_parent()
+	player.set_physics_process(false)
+	
 func _on_AnimationPlayer_animation_finished(anim_name):
 	print("player attacked")
+	player.global_position = global_position
+	player.set_physics_process(true)
 	queue_free()
 	
 
+func _physics_process(delta):
+	player.global_position = $Sword.global_position
 
 func _on_Sword_area_entered(area):
-	area.owner.call("damage", 15)
+	if area.collision_layer == 2:
+		area.owner.call("damage", 15)
+	player.set_physics_process(true)
+	queue_free()
