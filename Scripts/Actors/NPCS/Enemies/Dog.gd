80b8b6dc672387191e1299033e6a84d0d58ae2f3
jamnk <pancakes3825@gmail.com>
2023-12-24 22:39:43 -0500

    shower time

--- Scripts/Actors/NPCS/Enemies/Dog.gd
+++ Scripts/Actors/NPCS/Enemies/Dog.gd
@@ -1,12 +1,10 @@
 extends RigidActor
 
 
-var target
-
 func _ready():
-	sprite = $Bloodsigilplaceholder
+	sprite = $CollisionShape2D/DogSprite
 	speed = 300
 
 func _integrate_forces(state):
+	$CollisionShape2D.global_rotation_degrees =0
 	linear_velocity = global_position.direction_to(PlayerAutoload.playerPos) * speed
-	print(linear_velocity)
