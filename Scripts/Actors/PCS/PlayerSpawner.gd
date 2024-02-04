ed21851882f748d65ba266fe4493b1923ea6f132
jamnk <pancakes3825@gmail.com>
2024-02-04 00:40:08 -0500

    newfound motivation

--- Scripts/Actors/PCS/PlayerSpawner.gd
+++ Scripts/Actors/PCS/PlayerSpawner.gd
@@ -6,14 +6,16 @@ extends Node2D
 @export var Angel: PackedScene
 
 func _ready():
-	set_position(MapAutoload.spawnPoint)
-	print("spawnin")
-	match PlayerAutoload.playerCharacter:
-		1:
-			add_child(Priest.instantiate())
-		2:
-			add_child(Knight.instantiate())
-		3:
-			add_child(Witch.instantiate())
-		4:
-			add_child(Angel.instantiate())
+	if MapAutoload.spawnPoint:
+		set_position(MapAutoload.spawnPoint)
+	else:
+		print("spawnin")
+		match PlayerAutoload.playerCharacter:
+			1:
+				add_child(Priest.instantiate())
+			2:
+				add_child(Knight.instantiate())
+			3:
+				add_child(Witch.instantiate())
+			4:
+				add_child(Angel.instantiate())
