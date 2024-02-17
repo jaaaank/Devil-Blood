d6bddf28db5237f09eb60cb8b02ba1a98cf1b808
jamnk <pancakes3825@gmail.com>
2024-02-16 23:16:58 -0500

    WOOOOOOOO boss done functionally

--- Scripts/Actors/NPCS/Enemies/GiantMass.gd
+++ Scripts/Actors/NPCS/Enemies/GiantMass.gd
@@ -2,6 +2,8 @@ extends mass
 
 @onready var animP = $AnimationPlayer
 @export var tendril: PackedScene
+@export var baby: PackedScene
+@export var shockwave: PackedScene
 
 func _ready():
 	super()
@@ -13,17 +15,27 @@ func _ready():
 #1: Tendril
 #2: Summon babies
 func attack():
-	var attackChosen = randi_range(0,1)
+	var attackChosen = randi_range(0,2)
 	match attackChosen:
 		0:
 			animP.play("jumpajumpa")
 			await animP.animation_finished
+			animP.play("RESET")
 		1:
 			$Aim.look_at(PlayerAutoload.playerPos)
 			$Aim.add_child(tendril.instantiate())
 		2:
-			pass
-		3:
-			print("didnt attack")
-			pass
+			var randomPos = [Vector2(-12000,-500), Vector2(-12000, 1500), Vector2(-10500, 1500), Vector2(-10500, -500), Vector2(-11500, 1000), Vector2(-11500, 0)]
+			for i in randomPos:
+				var b = baby.instantiate()
+				add_sibling(b)
+				b.global_position = i
+		#3:
+			#print("didnt attack")
+			#pass
 	$attackTimer.start(randi_range(2,5))
+
+func spawnShockwave():
+	var pee = shockwave.instantiate()
+	add_sibling(pee)
+	pee.global_position = global_position
