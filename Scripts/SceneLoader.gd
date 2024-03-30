123b3538d12c9221ed32cdfcbaa51fe4b9b5f78c
jamnk <pancakes3825@gmail.com>
2024-03-30 08:17:44 -0400

    connect outside and inside shelter

--- Scripts/SceneLoader.gd
+++ Scripts/SceneLoader.gd
@@ -12,5 +12,5 @@ func _on_SceneLoader_body_entered(_body):
 # warning-ignore:return_value_discarded
 	get_tree().change_scene_to_file(nextScene)
 	
-func _get_configuration_warnings() -> PackedStringArray:
-	return ["Next scene or new spawn point is empty fucknuts"] if not nextScene or newSpawnPoint == Vector2.ZERO  else [""]
+#func _get_configuration_warnings() -> PackedStringArray:
+	#return ["Next scene or new spawn point is empty fucknuts"] if not nextScene or newSpawnPoint == Vector2.ZERO  else [""]
